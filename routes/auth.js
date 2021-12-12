0bdea26b0ad8d9a9c6ae83935ed5e40486c66546
ruroni1 <47061526+ruroni1@users.noreply.github.com>
2021-12-12 12:46:58 +0530

    dd

--- routes/auth.js
+++ routes/auth.js
@@ -5,13 +5,14 @@ var router=express.Router();
 router.use(express.json())
 
 router.post('/signupOrlookup',(req,res)=>{
+    console.log(req.body)
     User.findOne({email : req.body.email}, (err, user) =>{
         if(err) return res.send( { message : 'unable to look up right now', code : 0})
         else if(user) return res.send( {message : 'a user with this name exists', code : 1})
         else{
             // user not exists , register this user
             let nuser=new User({
-                email:req.body.email,
+                email:     req.body.email,
                 password : req.body.password
             });
             nuser.save()
